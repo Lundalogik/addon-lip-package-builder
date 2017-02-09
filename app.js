945918a408411654e30a0e316ff2f96b5cdb35f6
RazeTheRuler <r.alestigthunborg@gmail.com>
2017-02-09 13:53:48 +0100

    Refactored

--- app.js
+++ app.js
@@ -34,8 +34,13 @@ lbs.apploader.register('LIPPackageBuilder', function () {
 
 
         vm.selectTables.subscribe(function(newValue){
-            ko.utils.arrayForEach(vm.filteredTables(),function(item){
-                item.select();
+            ko.utils.arrayForEach(vm.filteredTables(),function(table){
+                 ko.utils.arrayForEach(table.guiFields(),function(field){ 
+                     if (field.selected() != newValue){
+                         field.selected(newValue);
+                     }
+                 });
+             table.indeterminate(table.getIndeterminate());
             });
         });
 
